1a8d944293d1e371b5ccbcdcb3c094d226a44bba
Nipel-Crumple <warlen94@mail.ru>
2015-02-24 23:48:09 +0300

    Trying to test hibernate+spring

---
+++ src/test/java/com/codegroup/challengecloud/dao/SubscribeTest.java
@@ -0,0 +1,22 @@
+package com.codegroup.challengecloud.dao;
+
+import com.codegroup.challengecloud.dao.impl.SubscriptionDaoMySQL;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Nipel-Crumple on 24.02.2015.
+ */
+public class SubscribeTest extends BaseTest {
+
+    /*@Resource
+    private SubscriptionDaoMySQL subscriptionDao;
+
+    @Test
+    public void testFindById() {
+        Assert.assertNotNull(subscriptionDao.findById("1"));
+        System.out.println(subscriptionDao.findById("1").getDate());
+    }*/
+}
