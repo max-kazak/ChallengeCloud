3d675d7673831f499f277acdc1f2a55fede794cd
volterr <volterr@gmail.com>
2015-02-28 20:32:28 +0300

    join test

--- src/test/java/com/codegroup/challengecloud/dao/SubscriptionTest.java
+++ src/test/java/com/codegroup/challengecloud/dao/SubscriptionTest.java
@@ -2,16 +2,27 @@ package com.codegroup.challengecloud.dao;
 
 import com.codegroup.challengecloud.dao.impl.SubscriptionDaoMySQL;
 import com.codegroup.challengecloud.services.SubscriptionService;
+
 import junit.framework.Assert;
+
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
 /**
  * Created by Nipel-Crumple on 24.02.2015.
  */
-public class SubscriptionTest extends BaseTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class SubscriptionTest{
 
+	@Autowired
+    public SessionFactory sessionFactory;
     @Resource
     private SubscriptionService subscriptionService;
 
