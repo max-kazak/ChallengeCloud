952f79f05682eaa0dbda4384aee1fd724d94e616
Vladimir Zhdanov <LaimZ@ya.ru>
2015-03-09 03:12:55 +0300

    Freemaker Works!

--- src/main/java/com/codegroup/challengecloud/controllers/ChallengesController.java
+++ src/main/java/com/codegroup/challengecloud/controllers/ChallengesController.java
@@ -1,19 +1,43 @@
 package com.codegroup.challengecloud.controllers;
  
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+
+
+
+
+
+
 
 //import com.codegroup.challengecloud.model.User;
 //import com.codegroup.challengecloud.services.UserService;
 import com.codegroup.challengecloud.model.Challenge;
 import com.codegroup.challengecloud.services.ChallengeService;
+
+
+
+
+
+
+
+/* For FreeMarker */
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
 
 /**
 * @author Vladimir Zhdanov
@@ -23,6 +47,7 @@ import com.codegroup.challengecloud.services.ChallengeService;
  
 @Controller
 public class ChallengesController {
+	private static final Logger log = Logger.getLogger(HomeController.class);
     private Challenge challenge;
     private ChallengeService challengeService;
  
@@ -44,8 +69,32 @@ public class ChallengesController {
     
     @RequestMapping(value = "/challenges-all", method = RequestMethod.GET)
     public @ResponseBody
-    String getAllChallenges() {
+    String getAllChallenges() throws Exception {
+		log.info("getAllChallenges().");
+    	String code = "<p> Internal Error! </p>";
     	
-        return "Hello!!!";
+    	Configuration cfg = new Configuration();
+        cfg.setClassForTemplateLoading(ChallengesController.class, "/");// TODO /templates
+        
+        Map<String, Object> input = new HashMap<String, Object>();
+        input.put("challengeName", "Challenge 1");
+
+        StringWriter stringWriter;
+        try {
+			Template template = cfg.getTemplate("challenge.ftl");
+	        stringWriter = new StringWriter();
+	        try {
+	        	template.process(input, stringWriter);
+	        } catch (TemplateException e2) {
+				throw new Exception("ChallengesController.getAllChallenges: TemplateExcepon: "+e2.toString());
+			} finally {
+				stringWriter.close();
+				code = stringWriter.toString();
+	        }
+		} catch (IOException e) {
+			throw new IOException("ChallengesController.getAllChallenges: Can't load template challenge.ftl: "+e.toString());
+		}
+		log.info("getAllChallenges() returns [" + code + "].");
+        return code;
     }
 }
---
+++ src/main/resources/challenge.ftl
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1 style="font-color: #FF64FF;"> challenge.ftl ${challengeName} </h1>
+</body>
+</html>
