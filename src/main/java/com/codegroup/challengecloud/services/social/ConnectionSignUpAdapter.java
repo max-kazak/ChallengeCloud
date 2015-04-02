09331f1b282e64d854ccde1cf6b4cff9c83f91ff
Nipel-Crumple <warlen94@mail.ru>
2015-04-02 21:03:56 +0300

    Revert in Twitter changes.

--- src/main/java/com/codegroup/challengecloud/services/social/ConnectionSignUpAdapter.java
+++ src/main/java/com/codegroup/challengecloud/services/social/ConnectionSignUpAdapter.java
@@ -11,42 +11,32 @@ import com.codegroup.challengecloud.model.User;
 import com.codegroup.challengecloud.services.UserService;
 
 @Service
-public class ConnectionSignUpAdapter implements ConnectionSignUp{
+public class ConnectionSignUpAdapter implements ConnectionSignUp {
 
-	private static final Logger log = Logger.getLogger(ConnectionSignUpAdapter.class);
-	
-	@Autowired
-	UserService userService;
-	
-	@Override
-	public String execute(Connection<?> connection) {
-		
-		UserProfile userProfile = connection.fetchUserProfile();
-		if(userProfile == null) {
-			log.error("can't fetch user profile from connection");
-			return null;
-		}
+    private static final Logger log = Logger.getLogger(ConnectionSignUpAdapter.class);
 
-		//TODO::here it is absence of email merging profiles
-		User userExistsLogin = userService.findByLogin(userProfile.getUsername());
-		User newUser;
-		if (userExistsLogin == null) {
-			log.debug("UserProfile email: " + userProfile.getEmail());
-			newUser = userService.createProfile(userProfile.getUsername(),
-					userProfile.getEmail(),
-					null,
-					userProfile.getName());
-			log.debug("from SignUpAdapter: created user from Provider: " +
-					connection.getApi().toString() +
-					" with email = " + newUser.getEmail());
-		} else {
-			log.debug("from SignUpAdapter: user already exists, try to unite profiles, id = " +
-					userExistsLogin.getId() +
-					"provider: " + connection.getApi().toString());
-			return userExistsLogin.getId();
-		}
-		
-		return newUser.getId();
-	}
+    @Autowired
+    UserService userService;
+
+    @Override
+    public String execute(Connection<?> connection) {
+
+        UserProfile userProfile = connection.fetchUserProfile();
+        if (userProfile == null) {
+            log.error("can't fetch user profile from connection");
+            return null;
+        }
+
+        User user = userService.createProfile(userProfile.getUsername(),
+                userProfile.getEmail(),
+                null,
+                userProfile.getName());
+
+        log.debug("from SignUpAdapter: created user from Provider: " +
+                connection.getApi().toString() +
+                " with email = " + user.getEmail());
+        return user.getId();
+
+    }
 
 }
