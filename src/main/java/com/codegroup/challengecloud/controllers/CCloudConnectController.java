c03b1bb4e5fbca6265bbf446ff7786ac069ab37b
Nipel-Crumple <warlen94@mail.ru>
2015-04-11 17:11:50 +0300

    Fixed bug with redirecting to twitterConnect after sign up

--- src/main/java/com/codegroup/challengecloud/controllers/CCloudConnectController.java
+++ src/main/java/com/codegroup/challengecloud/controllers/CCloudConnectController.java
@@ -5,6 +5,8 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
  * Created by Nipel-Crumple on 04.04.2015.
  */
 public class CCloudConnectController extends ConnectController{
+
     @Inject
     public CCloudConnectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
         super(connectionFactoryLocator, connectionRepository);
@@ -25,4 +28,15 @@ public class CCloudConnectController extends ConnectController{
         String path = "/settings";
         return new RedirectView(path, true);
     }
+
+    @Override
+    protected String connectView(String providerId) {
+        return "/signinup";
+    }
+
+    @Override
+    protected String connectedView(String providerId) {
+        return "/signinup";
+    }
+
 }
