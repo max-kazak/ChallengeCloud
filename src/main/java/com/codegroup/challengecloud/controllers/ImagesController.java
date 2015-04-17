eb8cb028cc8a2919ee67775bbca0522cb75433cc
Vladimir Zhdanov <LaimZ@ya.ru>
2015-04-17 06:47:43 +0300

    After uploading redirects back

--- src/main/java/com/codegroup/challengecloud/controllers/ImagesController.java
+++ src/main/java/com/codegroup/challengecloud/controllers/ImagesController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.codegroup.challengecloud.model.Challenge;
 import com.codegroup.challengecloud.model.Image;
 import com.codegroup.challengecloud.services.ImageService;
 
@@ -19,6 +20,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+
+
+
+
 
 
 
@@ -47,6 +52,10 @@ public class ImagesController {
         //return new ModelAndView("images-managing", "message", "Images");
         return new ModelAndView("images-managing", map);
     }
+    
+    //TODO: make it jsp, not ftl. and use <c:url...>
+    // jsp does'n work, so I can't use <c:url...> 17.04.2015 by Vladimir Zhdanov
+    private static final String IMAGES_MANAGING_URL = "/challengecloud/images-managing.html";
 
     /**
      * @author Vladimir Zhdanov
@@ -101,6 +110,40 @@ public class ImagesController {
         return code;
     }
 
+    /**
+     * This method returns images-managing-upload page as String, because I can't return ModelAndView, just String
+     * @param text
+     * @return
+     */
+    String imagesManagingUploadPage(String text) {
+        log.info("imagesManagingUploadPage()");
+        String code = "<p> Internal Error! </p>";
+        Map<String, Object> input = new HashMap<String, Object>();
+        input.put("images_managing", IMAGES_MANAGING_URL);
+        StringWriter stringWriter;
+        Configuration cfg = new Configuration();
+        
+        cfg.setClassForTemplateLoading(ChallengesController.class, "/");// TODO /templates
+        
+        try {
+            Template template = cfg.getTemplate("images-managing-upload.ftl");
+            stringWriter = new StringWriter();
+            try {
+                //input.clear();
+                input.put("upload_status", text);
+                template.process(input, stringWriter);
+            } catch (TemplateException e2) {
+                log.error("Template Exception.");
+            } finally {
+                stringWriter.close();
+                code = stringWriter.toString();
+            }
+        } catch (IOException e) {
+            log.error("Can't load template: IOException.");
+        }
+        return code;
+    }
+    
     // Example https://spring.io/guides/gs/uploading-files/
     // Created on 09.04.2015 by Vladimir Zhdanov
     @RequestMapping(value="/images-managing-upload", method=RequestMethod.GET)
@@ -110,18 +153,20 @@ public class ImagesController {
 
     @RequestMapping(value = "/images-managing-upload", method = RequestMethod.POST)
     public @ResponseBody String upload(@RequestParam("file") MultipartFile file){
+    	Map <String,String> map = new HashMap();
+    	// Because ModelAndView for unknown reason doesn't work, let's use FreeMarker:   
     	if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
                 //TODO What to do with size???
-                log.debug("Befre saving an image");
+                log.debug("Before saving an image");
                 imageService.createImage(file.getName(), 'S', bytes);
-                return "You successfully uploaded " + file.getName() + "!";
+                return imagesManagingUploadPage("You successfully uploaded " + file.getName() + "!");
             } catch (Exception e) {
-                return "You failed to upload " + file.getName() + " => " + e.getMessage();
+                return imagesManagingUploadPage("You failed to upload " + file.getName() + " => " + e.getMessage());
             }
         } else {
-            return "You failed to upload " + file.getName() + " because the file was empty.";
+        	return imagesManagingUploadPage("You failed to upload " + file.getName() + " because the file was empty."); 
         }
-        }
+    }
 }
