0d2ed233319994f7ee68175239c7fcf05ed9b827
Nipel-Crumple <warlen94@mail.ru>
2015-04-22 20:30:08 +0300

    Scheduler worker was deleted. Now functionality in ScheduleConfig

--- src/main/java/com/codegroup/challengecloud/config/ScheduleConfig.java
+++ src/main/java/com/codegroup/challengecloud/config/ScheduleConfig.java
@@ -1,9 +1,13 @@
 package com.codegroup.challengecloud.config;
 
-import com.codegroup.challengecloud.services.scheduler.ScheduleWorker;
-import org.springframework.context.annotation.Bean;
+import com.codegroup.challengecloud.services.TwitterDownloadService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.inject.Inject;
 
 /**
  * Created by Nipel-Crumple on 08.04.2015.
@@ -11,9 +15,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
+    private static Logger log = Logger.getLogger(ScheduleConfig.class);
 
-    @Bean
-    public ScheduleWorker scheduleTaskService() {
-        return new ScheduleWorker();
+    @Autowired
+    TwitterDownloadService twitterDownloadService;
+
+    /**
+     * Every 5 minutes adding twitterPosts
+     * @author Nipel-Crumple
+     */
+    @Scheduled(cron="0 0/2 * * * ?")
+    public void scheduleTwitterDownloadPosts() {
+        log.debug("Launching download posts task");
+        twitterDownloadService.addAllUsersTwitterPosts();
     }
 }
