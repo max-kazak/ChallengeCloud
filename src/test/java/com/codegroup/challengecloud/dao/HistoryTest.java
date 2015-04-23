07c6a79cc5d39a8e27f4feb404efe8a5248cc7d4
Yefim-Krokhin <pechenkaya@mail.ru>
2015-04-23 20:05:50 +0300

    New test for History added

--- src/test/java/com/codegroup/challengecloud/dao/HistoryTest.java
+++ src/test/java/com/codegroup/challengecloud/dao/HistoryTest.java
@@ -1,6 +1,10 @@
 package com.codegroup.challengecloud.dao;
 
+import com.codegroup.challengecloud.dao.impl.HistoryDaoMySQL;
+import com.codegroup.challengecloud.model.History;
+import com.codegroup.challengecloud.model.User;
 import com.codegroup.challengecloud.services.HistoryService;
+import com.codegroup.challengecloud.services.UserService;
 import junit.framework.Assert;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
@@ -10,6 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by Krokhin on 21.04.2015.
@@ -18,23 +24,53 @@ import javax.annotation.Resource;
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class HistoryTest {
     private static final String EXCPECTED_EVENT = "1";
-    private static final String EXCPECTED_ID = "123";
+    private static final String FIRST_EXCPECTED_ID = "12";
+    private static final String SECOND_EXCPECTED_ID = "123";
     private static final String EXCPECTED_REF_ID = "577231985401290754";
+    private static final String USER_ID = "d8fbf948694caf35";
 
     @Autowired
     public SessionFactory sessionFactory;
 
+    @Autowired
+    public HistoryDao historyDao;
+
     @Resource
     private HistoryService historyService;
+
+    @Resource
+    private UserService userService;
 
 
     @Test
     public void testFindByRefId() {
         Assert.assertEquals(EXCPECTED_REF_ID, historyService.findByRefId(EXCPECTED_REF_ID).getRefId());
     }
+
     @Test
     public void testFindById() {
-        Assert.assertNotNull(historyService.findById(EXCPECTED_ID));
-        Assert.assertEquals(EXCPECTED_ID, historyService.findById(EXCPECTED_ID).getId());
+        Assert.assertNotNull(historyService.findById(SECOND_EXCPECTED_ID));
+        Assert.assertEquals(SECOND_EXCPECTED_ID, historyService.findById(SECOND_EXCPECTED_ID).getId());
+    }
+
+    @Test
+    public void testFindHistoryForUser() {
+        User user = userService.findById(USER_ID);
+        Set<History> allNotes = historyDao.getHistoryForUser(user);
+        Assert.assertNotNull(allNotes);
+        Iterator<History> iterator = allNotes.iterator();
+        int amount = 0;
+        while (iterator.hasNext()) {
+            if (amount < 2) {
+                History tempHistory = iterator.next();
+                if (amount == 0) {
+
+                    Assert.assertEquals(tempHistory.getId(), FIRST_EXCPECTED_ID);
+                } else {
+                    Assert.assertEquals(tempHistory.getId(), SECOND_EXCPECTED_ID);
+                }
+                amount++;
+            } else break;
+        }
     }
 }
